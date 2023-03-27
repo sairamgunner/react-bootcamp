6a6ec7070915b409585ee842fb212b31cfa3c1b3
sairamgunner <sairamgunner@gmail.com>
2023-03-27 19:00:17 -0400

    Initial Commit for Music Master

--- music-master/src/App.js
+++ music-master/src/App.js
@@ -1,47 +1,9 @@
 import React, { Component } from 'react';
-import Projects from './components/Projects';
-import Title from './components/Title';
-import SocialProfiles from './components/SocialProfiles';
-import profile from './assets/profile.jpg';
 
 class App extends Component {
-    state = { displayBio: false }
-
-    readMore() {
-        this.setState({ displayBio: true });
-    }
-
-    toggleDisplayBio = () => {
-        this.setState({ displayBio: !this.state.displayBio });
-    }
-
     render() {
-        let bio = this.state.displayBio ? (
-            <div>
-                <p>I love to code</p>
-                <p>I live in Whitby, ON</p>
-                <p>I want to make exceptional quality web applications</p>
-                <button onClick={this.toggleDisplayBio}>Collapse</button>
-            </div>
-        ) : (
-            <div>
-                <button onClick={this.toggleDisplayBio}>Read More</button>
-            </div>
-        );
-
         return (
-            <div>
-                <img src={profile} className='profile' alt='profile' />
-                <h1>Hello!</h1>
-                <p>My name is Sairam.</p>
-                <Title />
-                <p>Always looking forward to working on meaningful projects.</p>
-                {bio}
-                <hr />
-                <Projects />
-                <hr />
-                <SocialProfiles />
-            </div>
+            <div>React App</div>
         );
     }
 }
--- music-master/src/index.js
+++ music-master/src/index.js
@@ -1,18 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
 import App from './App';
-import Jokes from './components/Jokes';
 import './index.css';
-import Header from './components/Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <BrowserRouter history={createBrowserHistory()}>
-        <Routes>
-            <Route path='/' element={<Header><App /></Header>} />
-            <Route path='/jokes' element={<Header><Jokes /></Header>} />
-        </Routes>
-    </BrowserRouter>
-);
+
+root.render(<App />);
