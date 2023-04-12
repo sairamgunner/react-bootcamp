80790cbd87d25a22086f0064797ab9ffa4514580
sairamgunner <sairamgunner@gmail.com>
2023-04-11 21:27:29 -0400

    Progress on Tracks.js

--- music-master/src/components/Tracks.js
+++ music-master/src/components/Tracks.js
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
 
 class Tracks extends Component {
-    state = { playing: false, audio: null };
+    state = { playing: false, audio: null, playingPreviewUrl: null };
 
     toggleAudio = previewUrl => () => {
         const audio = new Audio(previewUrl);
         if(!this.state.playing){
             audio.play();
-            this.setState({ playing: true, audio } );
+            this.setState({ playing: true, audio, playingPreviewUrl: previewUrl });
         } else {
-            audio.pause();
+            this.state.audio.pause();
             this.setState({ playing: false } );
+
+            if(this.state.playingPreviewUrl != previewUrl) {
+                audio.play();
+                this.setState({ playing: true, audio, playingPreviewUrl: previewUrl })
+            }
         }
     }
 
@@ -24,8 +29,20 @@ class Tracks extends Component {
                     const { id, album, name, preview_url } = track;
 
                     return(
-                        <div key={id} onClick={this.toggleAudio(preview_url)}>
-                            <img src={album.images[0].url} alt='track-image'/>
+                        <div 
+                        key={id} 
+                        onClick={this.toggleAudio(preview_url)}
+                        style={{ 
+                            display: 'inline-block'
+                         }}>
+                            <img 
+                            src={album.images[0].url} 
+                            alt='track-image'
+                            style={{ 
+                                height:200, 
+                                width:200,
+                                objectFit: 'cover'
+                             }}/>
                             <p>{name}</p>
                         </div>
                     )
